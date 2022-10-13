b5180db8c3e88f4ea3eb34cf8c967709e978e3a5
Michael Stramel <mstramel@amazon.com>
2022-10-13 14:59:34 -0500

    WIP: Push to Vercel

--- demo/astro.config.mjs
+++ demo/astro.config.mjs
@@ -1,5 +1,15 @@
-
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  adapter: vercel(),
+  vite: {
+    ssr: {
+      external: ['@iconify/json']
+    }
+  }
+});
