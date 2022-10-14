e1277aad99b061d452b67a1a4de2e433e66f761f
Michael Stramel <mstramel@amazon.com>
2022-10-14 10:15:58 -0500

    WIP: Update to v2 icon name

--- demo/astro.config.mjs
+++ demo/astro.config.mjs
@@ -7,7 +7,15 @@ import vercel from "@astrojs/vercel/serverless"; // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel({
-    includeFiles: ['../node_modules/@iconify/json/json/heroicons.json', '../node_modules/@iconify/json/json/radix-icons.json']
+    includeFiles: [
+      '../node_modules/@iconify/json/json/heroicons.json',
+      '../node_modules/@iconify/json/json/radix-icons.json',
+      '../node_modules/@iconify/json/json/bi.json',
+      '../node_modules/@iconify/json/json/ic.json',
+      '../node_modules/@iconify/json/json/fe.json',
+      '../node_modules/@iconify/json/json/ri.json',
+      '../node_modules/@iconify/json/json/mdi.json',
+    ]
   }),
   // adapter: node(
   //   {
